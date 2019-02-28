62e48bb6b9634184687e705f1f0c8ccb2c2491cb
Daniel Rosenberg <drosen@google.com>
2019-02-28 13:21:07 -0800

    sm8250-common: Setup checkpoint_gc for /vendor
    Test: start checkpointing, observe that checkpointing happens
    Bug: 111020314
    Change-Id: Ic090ac2d0ca9a7dd5485f5ab2a967977cd3ab6b8

--- common.mk
+++ common.mk
@@ -63,6 +63,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
     otapreopt_script
 
