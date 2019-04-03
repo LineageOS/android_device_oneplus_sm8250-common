f07e32ed1db1db55d904b1b81ccf420826734338
Bowgo Tsai <bowgotsai@google.com>
2019-04-03 16:44:10 +0800

    sm8250-common: Adding GSI keys
    Bug:112293933
    Test: build and checks the keys are installed into ramdisk
    Change-Id: I216f3900e9511e0929418af050ff964898d0aea1

--- common.mk
+++ common.mk
@@ -17,6 +17,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
