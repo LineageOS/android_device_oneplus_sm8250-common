1dd29a35a02c10e36587d09e7afa114cc5938214
LuK1337 <priv.luk@gmail.com>
2019-08-05 21:06:30 +0200

    sm8250-common: Unset BOARD_VNDK_VERSION
    Change-Id: Ie9e8615eb7fe186dbb595fc3fa0d2bfc013b5d47

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,9 +69,6 @@ TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
-# Treble
-BOARD_VNDK_VERSION := current
-
 # A/B
 AB_OTA_UPDATER := true
 
