48f5720c6ced9032c8c0ea58120cb367e955fc80
LuK1337 <priv.luk@gmail.com>
2020-11-19 15:18:15 +0100

    sm8250-common: Add kebab to build guards

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter instantnoodle instantnoodlep,$(TARGET_DEVICE)),)
+ifneq ($(filter instantnoodle instantnoodlep kebab,$(TARGET_DEVICE)),)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,7 +34,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "instantnoodle instantnoodlep"
+write_headers "instantnoodle instantnoodlep kebab"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
