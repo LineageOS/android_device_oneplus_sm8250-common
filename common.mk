97c17043ca13251712a227d37aa6684e9f233b6e
LuK1337 <priv.luk@gmail.com>
2020-11-20 14:45:26 +0100

    sm8250-common: Import WifiResCommon from stock
    Change-Id: I009437c1b6d0d989a2441f6051706f5f885c0061

--- common.mk
+++ common.mk
@@ -223,6 +223,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# WiFi
+PRODUCT_PACKAGES += \
+    WifiResCommon
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
