61f786918b51f84b2cbc881d552dfbb24b03d68f
LuK1337 <priv.luk@gmail.com>
2020-11-20 13:23:05 +0100

    sm8250-common: Satisfy wfdservice dependencies

--- common.mk
+++ common.mk
@@ -110,11 +110,12 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig \
+    libdisplayconfig.qti \
     libqdMetaData \
     libqdMetaData.system \
     libvulkan \
-    vendor.display.config@1.0
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -147,6 +148,9 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
+
+PRODUCT_PACKAGES += \
+    libavservices_minijail
 
 # Net
 PRODUCT_PACKAGES += \
