055124938976d6d63bf88fe96676a51cda9272e6
LuK1337 <priv.luk@gmail.com>
2021-04-14 14:29:58 +0200

    sm8250-common: Import QTI vibrator HAL
    Change-Id: I6ed3252c6da746c8fb24f44b3b3ec2b1f32a6cb2

--- common.mk
+++ common.mk
@@ -471,7 +471,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.oneplus_kona
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
