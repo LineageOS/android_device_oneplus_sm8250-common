bb16435fa46061eea89909a7242870c6de563212
LuK1337 <priv.luk@gmail.com>
2021-05-04 19:45:43 +0200

    sm8250-common: Relocate volume steps customization to vendor.prop
    Change-Id: Ic0d0666de7899dde5afbd1cd40370ccea2837419

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,7 +61,6 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
