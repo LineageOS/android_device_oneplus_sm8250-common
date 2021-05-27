a5f8427ba9ca2b9d0f447c431876e27cd87bf7a1
Bruno Martins <bgcngm@gmail.com>
2021-05-27 19:06:40 +0100

    sm8250-common: Move WFD permissions and cleanup
    Change-Id: I5c8adf64e83a570f8c6a90ad7f082c78b707cb07

--- common.mk
+++ common.mk
@@ -516,7 +516,7 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-wfd.xml
 
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
