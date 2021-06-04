eb771b425ebdaf4372a76e1445dc127ccbfdf470
YumeMichi <do4suki@gmail.com>
2021-06-04 22:49:28 +0800

    sm8250-common: Add lemonades to build guards
    Change-Id: Ic8b5cbe0ab7ecdb5796e58c4ea96045b852e61b8

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter instantnoodle instantnoodlep kebab,$(TARGET_DEVICE)),)
+ifneq ($(filter instantnoodle instantnoodlep kebab lemonades,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "instantnoodle instantnoodlep kebab"
+write_headers "instantnoodle instantnoodlep kebab lemonades"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
