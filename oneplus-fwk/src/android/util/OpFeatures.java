8dbce668388d6793d1ad4bcab01e86bc657d9e3f
Bruno Martins <bgcngm@gmail.com>
2021-06-23 22:32:53 +0100

    sm8250-common: Update ODM features list from OSS 11.2.7.7
    Change-Id: Ic9138245adc8819c7662e933f3bbe6df062801e5

--- oneplus-fwk/src/android/util/OpFeatures.java
+++ oneplus-fwk/src/android/util/OpFeatures.java
@@ -381,8 +381,9 @@ public final class OpFeatures {
     public static final int OP_FEATURE_MM_MUTE_DELAY_SWITCH = 368;
     public static final int OP_FEATURE_ONLINE_CONFIG_NETWORK = 369;
     public static final int OP_FEATURE_KEEP_HIGH_VSYNC = 370;
+    public static final int OP_FEATURE_MM_SYNC_ACDB_ID_FROM_Q = 371;
 
-    private static final BitSet sFeatures = new BitSet(372);
+    private static final BitSet sFeatures = new BitSet(373);
 
     static {
         File file = new File("/odm/etc/odm_feature_list");
