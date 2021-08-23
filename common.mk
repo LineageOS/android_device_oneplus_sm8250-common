7ad5bbf53f5b0bcc0d876925ac7b8005cf53e3c1
LuK1337 <priv.luk@gmail.com>
2021-08-23 18:00:54 +0200

    sm8250-common: RIP `remove_packages`
    No longer necessary since now we can just pass `OVERRIDES` to extract
    utils and it'll handle it automatically for us :^)
    
    Change-Id: I19c97f5daa72d809dae2442fcb2b8b81a702aaf2

--- common.mk
+++ common.mk
@@ -392,10 +392,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
