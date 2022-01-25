e520915ac165be2eaf18ed36e204422e3ab1000d
LuK1337 <priv.luk@gmail.com>
2022-01-25 21:03:51 +0100

    sm8250-common: Switch to AOSP NfcNci
    Change-Id: I3403084373445475ee1d997c47cfaa887bcfeb4a

--- common.mk
+++ common.mk
@@ -374,17 +374,12 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0:64 \
-    android.hardware.nfc@1.1:64 \
-    android.hardware.nfc@1.2:64 \
     android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.0:64 \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
+    NfcNci \
     SecureElement \
-    Tag \
-    vendor.nxp.nxpese@1.0:64 \
-    vendor.nxp.nxpnfc@1.0:64
+    Tag
 
 # OMX
 PRODUCT_PACKAGES += \
