edecbe2e5f2d8d9d4a33d917f5d2d3971f0d27e2
LuK1337 <priv.luk@gmail.com>
2022-01-26 20:37:59 +0100

    sm8250-common: Update WFD system stack from lemonadep
    Change-Id: I728810877f3aa40a4fe068f23fd2cfc9194f8066

--- common.mk
+++ common.mk
@@ -240,8 +240,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.kona \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
+    libdisplayconfig.system.qti \
     libqdMetaData \
+    libqdMetaData.system \
     libsdmcore \
     libsdmutils \
     libtinyxml \
