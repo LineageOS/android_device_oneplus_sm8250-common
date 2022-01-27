53ec71b396899524cf5f7110a9cfbf837d56342b
Alexander Koskovich <akoskovich@pm.me>
2022-01-27 14:23:56 -0700

    sm8250-common: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- common.mk
+++ common.mk
@@ -191,7 +191,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    Snap \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Common init scripts
