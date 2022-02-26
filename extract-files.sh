21d1cbacbdd3cba52888f7c423e9a0e5da5483da
LuK1337 <priv.luk@gmail.com>
2022-02-26 13:39:46 +0100

    sm8250-common: Unlock gc5035 camera
    [DEBUG  ] JIANJIA.cpp:89 PretendOpenCamera() logicalCameraId 4, m_ClientPackageName 0
    [DEBUG  ] STELLA.cpp:1445 informKernel() force: -1 value: 6 buf: 5
    [DEBUG  ] chxextensionmodule.cpp:1005 ExtendOpen() You shall not pass 0
    [DEBUG  ] chxextensioninterface.cpp:298 chi_extend_open() [OP_EXT] OPEN_CAMERA X
    [ERROR][HAL    ] camxhal3module.cpp:772 ProcessCameraOpen() Camera Open failed with error status = CamxResultEInvalidArg
    
    Change-Id: I9b3e4e55f07cef40886e09941f7ed0523cdf8cfa

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,9 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
+        vendor/lib64/hw/com.qti.chi.override.so)
+            sed -i "s/gc5035/gc503x/g" "${2}"
+            ;;
     esac
 }
 
