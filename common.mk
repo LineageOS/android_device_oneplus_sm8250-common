180f6ad73cb95a7ba505baf62d598dbd29cf6969
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    sm8250-common: Update to Boot Control HAL v1.2
    Change-Id: Ib5d06c9b0c7e2ed000eb03a9bf18e4d78cc8740d

--- common.mk
+++ common.mk
@@ -103,9 +103,9 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 PRODUCT_PACKAGES += \
