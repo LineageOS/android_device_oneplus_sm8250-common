612bfc9c99ed590867f5975d0d3dcdac8588d258
LuK1337 <priv.luk@gmail.com>
2022-03-24 11:17:21 +0100

    sm8250-common: Autopatch vendor.qti.hardware.camera.postproc impl
    Change-Id: If9b730f83853a169066291c6787dd81d5609ac5e

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
             ;;
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "1F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
     esac
 }
 
