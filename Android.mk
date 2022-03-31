f5974f82c3da0caad871854e54631b1675ced0e2
LuK1337 <priv.luk@gmail.com>
2022-03-31 18:05:37 +0200

    Revert "sdm845-common: Make Android.mk search recursive"
    No longer needed.
    
    This reverts commit b56fd4bc01f46aced6a8d07b4510c8c88f152bf8.
    
    Change-Id: Iae9c4257a98dd4ba47ef9992b1dec133ccc56764

--- Android.mk
+++ Android.mk
@@ -17,8 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter instantnoodle instantnoodlep kebab,$(TARGET_DEVICE)),)
-subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
