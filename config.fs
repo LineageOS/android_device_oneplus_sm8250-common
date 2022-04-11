a2c8c0b6247df618e6ac3b519ed3e58b40617296
LuK1337 <priv.luk@gmail.com>
2022-04-11 09:51:02 +0200

    sm8250-common: Regenerate config.fs
    Change-Id: I73e85d9d33f87dafef930bad0c2d7c08ef31d7c2

--- config.fs
+++ config.fs
@@ -22,6 +22,63 @@ value:2907
 [AID_VENDOR_FASTRPC]
 value:2908
 
+[AID_VENDOR_QTR]
+value:2909
+
+[AID_VENDOR_LAUNCHER]
+value:2988
+
+[AID_VENDOR_SAURES]
+value:2989
+
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
+caps: 0
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[system/bin/cnss-daemon]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: NET_BIND_SERVICE
+
+[system/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [system/vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
