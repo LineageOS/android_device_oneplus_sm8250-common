d5ab1731e10f3abb1d6b9b7717d2af2d945c8bda
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 00:02:42 -0700

    sm8250-common: Import QCOM scripts from LA.UM.9.12.r1-14300.01-SMxx50.QSSI13.0
    Change-Id: Ia624b92cbbaeaf240958df2f640140d79f9f9335

--- common.mk
+++ common.mk
@@ -277,6 +277,17 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Init
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.rc \
+    init.qcom.recovery.rc \
+    init.qcom.sh \
+    init.target.rc \
+    ueventd.qcom.rc
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
