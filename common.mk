3aa85bad3aa6c9465bb8a535be897dc939129b00
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 21:39:56 -0700

    sm8250-common: overlay: Import relevant custom changes made prior to RROs
    Change-Id: Id3e5c5548088209a2459217dcdfe78fb59ff7221

--- common.mk
+++ common.mk
@@ -36,6 +36,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    OPlusFrameworksResCommon \
+    OPlusSettingsResCommon \
     WifiResTarget
 
 # VNDK
