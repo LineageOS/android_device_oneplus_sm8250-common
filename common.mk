11b8ca1447c4d4a958e55d4a4cb2d32fc8755056
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 20:30:41 -0700

    sm8250-common: overlay: Atomize
    Change-Id: I972d46e1dafb03b42249193fd033e91c4e3b3bc5

--- common.mk
+++ common.mk
@@ -28,7 +28,6 @@ $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
@@ -430,7 +429,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
