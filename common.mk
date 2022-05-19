feee607292311959c4bb0c45ac20f16354c3a92f
Sebastiano Barrezi <barezzisebastiano@gmail.com>
2022-05-19 23:46:24 +0300

    sm8250-common: Drop TARGET_USES_QTI_CAMERA_DEVICE
    * qcom-camera topic hasn't been ported to 19.1
    * see: https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/331507
    * Keep building vendor.qti.hardware.camera.device@1.0 interface lib,
      IMS stack and possibly camera HAL still needs it
    
    Change-Id: Ifb053d215527d602109bbb451d96ec80cf028548

--- common.mk
+++ common.mk
@@ -194,6 +194,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Common init scripts
