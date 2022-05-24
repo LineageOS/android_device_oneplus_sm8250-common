947e09401d8a9436907ce9064724893d97503600
Alexander Koskovich <akoskovich@pm.me>
2022-05-24 06:16:11 -0700

    sm8250-common: Import GPS configuration from IN2025_11.C.21
    Change-Id: I64c78ddc6af425117af8dd85f2fc9a49ac3bb942

--- common.mk
+++ common.mk
@@ -192,6 +192,9 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Graphics
