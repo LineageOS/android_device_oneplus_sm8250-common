251a03831ba3b403b24d2f043ab0d69574533ca5
LuK1337 <priv.luk@gmail.com>
2022-06-01 19:31:13 +0200

    sm8250-common: Unlock aux cameras
    This basically NOPs update_camera_metadata_entry() call that sets
    ANDROID_REQUEST_AVAILABLE_CAPABILITIES_SYSTEM_CAMERA to 1 for aux cams.
    
    Change-Id: Ie869b3eb0ea85af99904e2e23e86c4cd0acc3596

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,10 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
+        vendor/lib64/hw/com.qti.chi.override.so)
+            "${SIGSCAN}" -p "9B 2B 04 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "46 23 04 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "1F 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
