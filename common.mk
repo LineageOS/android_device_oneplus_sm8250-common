582f1f41aa164d1d4cee641e0e8eaa557291c421
LuK1337 <priv.luk@gmail.com>
2022-06-22 20:13:49 +0200

    sm8250-common: Hacks for FOD
    Change-Id: I31db727d6c117060d1f3602d7253578590bd4038

--- common.mk
+++ common.mk
@@ -172,7 +172,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oplus
+    android.hardware.biometrics.fingerprint@2.3-service.oplus \
+    libshims_fingerprint.oplus
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
