baf8078e10e6b5fdd7cce0159be6a871e2f2bcf3
LuK1337 <priv.luk@gmail.com>
2022-07-16 22:24:16 +0200

    sm8250-common: Register IHighTouchPollingRate interface
    Change-Id: I3c48324cd201e810f04ccaa39898d7c8e43fa2e5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,6 +88,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+ODM_MANIFEST_FILES := $(COMMON_PATH)/manifest_odm.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_oplus
