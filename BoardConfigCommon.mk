7f264ef5559f5f3bf28ad87cf394d7a1ccdf8d25
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    sm8250-common: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-07-05
 
 # SEPolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
 # Verified Boot
