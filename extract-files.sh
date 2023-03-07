30848526a762e833f9dc97cfe6176ea3b8f5cc3b
LuK1337 <priv.luk@gmail.com>
2023-03-07 10:39:58 +0100

    sm8250-common: Remove libnfc-nxp.conf hacks
    These aren't required on up to date NFC FW.
    
    Change-Id: I181773e036002102c54256e9d7ec15b27a547287

--- extract-files.sh
+++ extract-files.sh
@@ -78,10 +78,6 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
-        vendor/etc/libnfc-nxp.conf)
-            sed -i "s/^NXP_RF_CONF_BLK_9/#NXP_RF_CONF_BLK_9/" "${2}"
-            sed -i "s/^NXP_RF_CONF_BLK_10/#NXP_RF_CONF_BLK_10/" "${2}"
-            ;;
         vendor/lib64/hw/com.qti.chi.override.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             sed -i "s/com.oem.autotest/\x00om.oem.autotest/" "${2}"
