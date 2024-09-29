de92f91c1e2340b5f0df5a5cae491a9ce4e5da25
Michael Bestas <mkbestas@lineageos.org>
2024-09-29 11:24:29 +0300

    sm8250-common: Sync extract scripts with 22 templates
    Change-Id: I7963ae050cfb193c5d0c2636da9c1e8f82bcdbdb

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -27,7 +27,7 @@ setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" tr
 write_headers "instantnoodle instantnoodlep kebab lemonades"
 
 # The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt"
 
 # Finish
 write_footers
@@ -41,7 +41,7 @@ if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
     write_headers
 
     # The standard device blobs
-    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" true
+    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt"
 
     if [ -f "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt" ]; then
         append_firmware_calls_to_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-firmware.txt"
