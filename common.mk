968fdd12f0b4de9579e38f65b89cf51fe51eac0b
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    sm8250-common: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- common.mk
+++ common.mk
@@ -200,6 +200,9 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
