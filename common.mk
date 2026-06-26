1c0869855b2915bf03b2b1684718b20da76d6323
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-26 01:19:38 -0400

    sm8250-common: fastbootd is now enabled by default
    Change-Id: Ieb54140550168f84ba4f067605fd6f7107979c31

--- common.mk
+++ common.mk
@@ -138,8 +138,7 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl.custom \
-    fastbootd
+    android.hardware.fastboot@1.1-impl.custom
 
 # Fingerprint
 PRODUCT_PACKAGES += \
